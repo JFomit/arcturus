a2baa404ccac21f38047f5299edf1048e167e580
JFomit <tzaberejny@gmail.com>
2025-10-12 17:50:32 +0300

    feat: initial completion

--- src/init.rs
+++ src/init.rs
@@ -9,9 +9,9 @@ use crate::bios::com::ComStatusFlags;
 use crate::stub::{conn::ComConnection, gdb::DosTarget};
 
 #[no_mangle]
-static mut DOS_TARGET: DosTarget = unsafe { core::mem::zeroed() };
-static mut GDB_STATE_MACHINE: Option<GdbStubStateMachine<'static, DosTarget, ComConnection>> = None;
-static mut BUF: [u8; 1024] = [0; 1024];
+pub static mut DOS_TARGET: DosTarget = unsafe { core::mem::zeroed() };
+pub static mut GDB_STATE_MACHINE: Option<GdbStubStateMachine<'static, DosTarget, ComConnection>> = None;
+pub static mut BUF: [u8; 1024] = [0; 1024];
 
 pub fn init_dbg() -> Result<(), i32> {
     unsafe { DOS_TARGET = DosTarget::new() };
@@ -27,61 +27,6 @@ pub fn init_dbg() -> Result<(), i32> {
 
     unsafe {
         GDB_STATE_MACHINE = Some(gdb.run_state_machine(&mut DOS_TARGET).map_err(|_| 2)?);
-    }
-
-    let res = loop {
-        unsafe {
-            GDB_STATE_MACHINE = match GDB_STATE_MACHINE.take().unwrap() {
-                GdbStubStateMachine::Idle(mut gdb) => {
-                    let mut byte = gdb.borrow_conn().read();
-                    loop {
-                        if byte.is_err_and(|_| {
-                            let flags = gdb.borrow_conn().status();
-                            flags.contains(ComStatusFlags::TimeOutError)
-                                | flags.contains(ComStatusFlags::TransmitterHoldingRegisterEmpty)
-                        }) {
-                            byte = gdb.borrow_conn().read();
-                            continue;
-                        }
-                        break;
-                    }
-
-                    match gdb.incoming_data(&mut DOS_TARGET, byte.unwrap()) {
-                        Ok(gdb) => Some(gdb),
-                        Err(e) => break Err(e),
-                    }
-                }
-                GdbStubStateMachine::Running(gdb) => {
-                    match gdb.report_stop(&mut DOS_TARGET, SingleThreadStopReason::DoneStep) {
-                        Ok(gdb) => Some(gdb),
-                        Err(e) => break Err(e),
-                    }
-                }
-                GdbStubStateMachine::CtrlCInterrupt(gdb) => {
-                    match gdb.interrupt_handled(&mut DOS_TARGET, None::<SingleThreadStopReason<u32>>) {
-                        Ok(gdb) => Some(gdb),
-                        Err(e) => break Err(e),
-                    }
-                }
-                GdbStubStateMachine::Disconnected(gdb) => break Ok(gdb.get_reason()),
-            }
-        }
-    };
-
-    match res {
-        Ok(disconnect_reason) => match disconnect_reason {
-            DisconnectReason::Disconnect => println!("GDB Disconnected"),
-            DisconnectReason::TargetExited(_) => println!("Target exited"),
-            DisconnectReason::TargetTerminated(_) => println!("Target halted"),
-            DisconnectReason::Kill => println!("GDB sent a kill command"),
-        },
-        Err(e) => {
-            if e.is_target_error() {
-                println!("Target raised a fatal error");
-            } else {
-                println!("{}", e);
-            }
-        }
     }
 
     Ok(())
--- src/stub/handlers.rs
+++ src/stub/handlers.rs
@@ -1,7 +1,108 @@
+use core::cell::UnsafeCell;
+
+use gdbstub::stub::{state_machine::GdbStubStateMachine, DisconnectReason, SingleThreadStopReason};
+
+use crate::{
+    _exit,
+    bios::com::ComStatusFlags,
+    init::{DOS_TARGET, GDB_STATE_MACHINE},
+};
 
 #[no_mangle]
 pub unsafe extern "C" fn break_handler() {
-    println!("int3");
+    DOS_TARGET.registers().eip -= 1;
+    send_stop();
+    let r = gdb_handler_loop();
+
+    match r {
+        Ok(true) => return,
+        Ok(false) => _exit(0),
+        Err(str) => panic!("{:?}", str),
+    }
 }
+
 #[no_mangle]
 pub unsafe extern "C" fn step_over_handler() {}
+
+fn send_stop() {
+    unsafe {
+        GDB_STATE_MACHINE = match GDB_STATE_MACHINE.take().unwrap() {
+            GdbStubStateMachine::Running(gdb) => {
+                match gdb.report_stop(&mut DOS_TARGET, SingleThreadStopReason::SwBreak(())) {
+                    Ok(gdb) => Some(gdb),
+                    Err(e) => {
+                        panic!("{:?}", e);
+                    }
+                }
+            }
+
+            gdb => Some(gdb),
+        }
+    }
+}
+
+fn gdb_handler_loop() -> Result<bool, &'static str> {
+    let res = loop {
+        unsafe {
+            GDB_STATE_MACHINE = match GDB_STATE_MACHINE.take().unwrap() {
+                GdbStubStateMachine::Idle(mut gdb) => {
+                    let mut byte = gdb.borrow_conn().read();
+                    loop {
+                        if byte.is_err_and(|_| {
+                            let flags = gdb.borrow_conn().status();
+                            flags.contains(ComStatusFlags::TimeOutError)
+                                | flags.contains(ComStatusFlags::TransmitterHoldingRegisterEmpty)
+                        }) {
+                            byte = gdb.borrow_conn().read();
+                            continue;
+                        }
+                        break;
+                    }
+
+                    match gdb.incoming_data(&mut DOS_TARGET, byte.unwrap()) {
+                        Ok(gdb) => Some(gdb),
+                        Err(e) => break Err(e),
+                    }
+                }
+                GdbStubStateMachine::Running(gdb) => {
+                    // match gdb.report_stop(&mut DOS_TARGET, SingleThreadStopReason::SwBreak(())) {
+                    //     Ok(gdb) => Some(gdb),
+                    //     Err(e) => break Err(e),
+                    // }
+                    println!("> Running");
+                    GDB_STATE_MACHINE = Some(gdb.into());
+                    return Ok(true);
+                }
+                GdbStubStateMachine::CtrlCInterrupt(gdb) => {
+                    match gdb
+                        .interrupt_handled(&mut DOS_TARGET, None::<SingleThreadStopReason<u32>>)
+                    {
+                        Ok(gdb) => Some(gdb),
+                        Err(e) => break Err(e),
+                    }
+                }
+                GdbStubStateMachine::Disconnected(gdb) => break Ok(gdb.get_reason()),
+            }
+        }
+    };
+
+    match res {
+        Ok(disconnect_reason) => {
+            match disconnect_reason {
+                DisconnectReason::Disconnect => println!("GDB Disconnected"),
+                DisconnectReason::TargetExited(_) => println!("Target exited"),
+                DisconnectReason::TargetTerminated(_) => println!("Target halted"),
+                DisconnectReason::Kill => println!("GDB sent a kill command"),
+            }
+
+            Ok(false)
+        }
+        Err(e) => {
+            if e.is_target_error() {
+                Err("Target raised a fatal error")
+            } else {
+                Err("Internal error")
+            }
+        }
+    }
+}
